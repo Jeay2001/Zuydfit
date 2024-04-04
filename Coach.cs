f8356757161304bf8c9df0aebe430e2c19c87380
lorenzo Pineda Olzheim <2304141pinedaolzheim@zuyd.nl>
2024-04-04 11:13:37 +0200

    alle klassen aangemaakt inclusief properties en constructors

--- Coach.cs
+++ Coach.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Zuydfit
 {
-    public class Coach
+    public class Coach : Person
     {
+        // Lijst van atleten die deze coach traint
+        public List<Athlete> Athletes { get; set; } = new List<Athlete>();
+
+        // Constructor
+        public Coach(int id, string firstName, string lastName, string streetName, string houseNumber, string postalCode)
+            : base(id, firstName, lastName, streetName, houseNumber, postalCode)
+        {
+            // Geen extra initialisatie nodig hier
+        }
     }
 }
--- Person.cs
+++ Person.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Zuydfit
 {
-    public class Person
+    public abstract class Person
     {
+        // Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public string HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+
+        // Constructor
+        public Person(int id, string firstName, string lastName, string streetName, string houseNumber, string postalCode)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+            PostalCode = postalCode;
+        }
     }
 }
