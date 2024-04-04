79854b8b2736e12dd72973950dc23619e4c19e1e
Bono Cichowski <2206279cichowski@zuyd.nl>
2024-04-04 10:36:33 +0200

    event class aanmaken

---
+++ Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zuydfit
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Duration { get; set; }
+
+    }
+}
