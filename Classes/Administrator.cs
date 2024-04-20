a96f441ee3729371b1a3160f52ac23f8116eecf8
Jeay <144847382+Jeay2001@users.noreply.github.com>
2024-04-20 18:19:34 +0200

    Update and rename administrator.cs to Administrator.cs

--- Classes/Administrator.cs
+++ Classes/Administrator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,7 @@ namespace Zuydfit
     internal class Administrator : Person
     {
         public List<Location> Locations { get; set; } = new List<Location>();
+        public Feedback Feedback { get; set; }
 
         public Administrator(int id, string firstName, string lastName, string streetName, string houseNumber, string postalcode, List<Location> locations)
             : base(id, firstName, lastName, streetName, houseNumber, postalcode)
